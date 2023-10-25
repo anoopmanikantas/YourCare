b0b2da51c0f5ae2f8d80c17e9d37b8ae12cc9fdc
anoopmanikantas <anoop.manikanta.21@gmail.com>
2023-10-25 14:30:04 -0400

    Introduce symptoms drop down

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import './App.css';
-import Penciltool from './components/PencilTool';
-import NavBar from './components/NavigationBar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PatientForm from './components/PatientDiagnosis';
-import SpeechToText from './components/SpeechToText';
-import Strings from './Strings';
 import { useState } from 'react';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import Strings from './Strings';
+import NavBar from './components/NavigationBar';
+import PatientForm from './components/PatientDiagnosis';
+import Penciltool from './components/PencilTool';
+import SpeechToText from './components/SpeechToText';
+ 
 function App() {
   const patients = {
-    name: ["joe", "Mason"],
-    id: ["00001", "00002"]
+    name: ["wow", "Mason"],
+    id: ["00001", "00002"],
+    symptoms: ["diabetes", "hypertension", "cancer", "Fever", "Rhinitis", "Migraine"]
   }
   const [patientData, setPatientData] = useState({
     name: Strings.emptyString,
-    id: Strings.emptyString
+    id: Strings.emptyString,
+    symptoms: Strings.emptyString,
   });
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<PatientForm patientData={patientData} setPatientData={setPatientData} patientsList={patients} />} />
-        <Route path={Strings.routeToWritingScreenString} element={<Penciltool />} />
-        <Route path={Strings.routeToSpeechToTextScreenString} element={<SpeechToText />} />
-      </Routes>
-    </BrowserRouter>
+<BrowserRouter>
+<NavBar />
+<Routes>
+<Route path="/" element={<PatientForm patientData={patientData} setPatientData={setPatientData} patientsList={patients} />} />
+<Route path={Strings.routeToWritingScreenString} element={<Penciltool />} />
+<Route path={Strings.routeToSpeechToTextScreenString} element={<SpeechToText />} />
+</Routes>
+</BrowserRouter>
   );
 }
-
+ 
 export default App;
